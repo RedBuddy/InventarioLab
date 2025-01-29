68c165f76f583981a7bc74975cd01c54947669b4
RedBuddy <67454532+RedBuddy@users.noreply.github.com>
2025-01-29 01:02:14 -0700

    Layout mas avanzado

--- src/app/shared/components/search/search.component.ts
+++ src/app/shared/components/search/search.component.ts
@@ -1,12 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, OnInit, OnDestroy } from '@angular/core';
+import { AuthService } from '../../../core/services/auth.service';
+import { Subscription } from 'rxjs';
+import { Router } from '@angular/router';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-search',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './search.component.html',
   styleUrl: './search.component.scss'
 })
-export class SearchComponent {
 
+export class SearchComponent {
+  isDropdownVisible = false;
+  isAuthenticated = false;
+  userRole: string | null = null;
+  userImage: string | null = null;
+  searchControl: FormControl = new FormControl('');
+
+
+  // constructor(private authService: AuthService, private router: Router) { }
+
+  searchFilter(): void {
+    const searchValue = this.searchControl.value;
+    if (searchValue) {
+      // this.router.navigate(['inicio/filtrar', searchValue]);
+    }
+  }
+
+  toggleDropdown() {
+    this.isDropdownVisible = !this.isDropdownVisible;
+  }
+
+  goToProfile() {
+    // const userId = this.authService.getUserIdFromToken();
+    // if (userId) {
+    //   this.router.navigate(['/perfil', userId]);
+    // }
+  }
+
+  logout() {
+    // this.authService.logout();
+  }
 }
